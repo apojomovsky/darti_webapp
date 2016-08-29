d7179cbce3a3095e94a9093ce9376528827289e3
Alexis Pojomovsky <apojomovsky@gmail.com>
2016-08-29 19:36:15 -0400

    Darti 7

--- darti.js
+++ darti.js
@@ -3,135 +3,9 @@ Datos= new Meteor.Collection("Datos");
 //Logs= new Meteor.Collection("Logs");
 Get= new Meteor.Collection("Get");
 Inbound = new Meteor.Collection("Inbound");
-Maps = new Meteor.Collection("Maps");
-Markers = new Mongo.Collection('Markers');
-
-Router.configure({
-	layoutTemplate:'main'
-});
-
-Router.route('/',{
-	name:'home',
-	template:'home'
-});
-
-Router.route("datos", function() {
-  var query = this.request.query;
-  this.response.statusCode = 200;
-  this.response.end('Recibido');
-  ayuda={
-	  dirviento:query.dirviento,
-	  velviento:query.velviento,
-	  lluvia:query.lluvia,
-	  temp_ext:query.temp_ext,
-	  temp_int:query.temp_int,
-	  humedad:query.humedad,
-	  presion:query.presion,
-	  gps:query.gps,
-	  fuente:query.fuente,
-	  estado:query.estado,
-	  fecha:new Date()
-  };
-  Meteor.call("cargarDatosGet",ayuda);
-  console.log(query);
-  // Do something with our found user here (see below).
-}, { where: "server" });
-
-Router.route('/informes',{
-	name:'informes',
-	template:'informes'
-});
-
-Router.route('/usuarios',{
-	name:'usuarios',
-	template:'usuarios'
-});
-
-Router.route('/marcadores',{
-	name:'marcadores',
-	template:'marcadores'
-});
-
-Router.route('/agregarMarcador',{
-	name:'agregarMarcador',
-	template:'agregarMarcador'
-});
-
-Router.route('/configuracion',{
-	name:'configuracion',
-	template:'configuracion'
-});
-
-Router.route('/exportarLogsEstacion',{
-	name:'exportarLogsEstacion',
-	template:'exportarLogsEstacion'
-});
-
-Router.route('/account', {
-  name: 'account',
-  template: 'account',
-  onBeforeAction: function(){
-    var currentUser = Meteor.userId();
-    if(currentUser){
-      this.next();
-    } else {
-      this.render("login");
-    }
-  }
-});
-
-Router.route('/inbound', function () {
-	var req = this.request;
-	var res = this.response;
-
-	var rawEmail = JSON.stringify(req.body.toString());
-	var emailSubjectSub = rawEmail.substring(rawEmail.search("Subject: ") + 9);
-	var emailSubject = emailSubjectSub.substring(0, emailSubjectSub.indexOf('\\n'));
-	var emailBodySub = rawEmail.substring(rawEmail.search("ltr") + 6);
-	var emailBody = emailBodySub.substring(0, emailBodySub.indexOf('<'));
-	var emailDateSub = rawEmail.substring(rawEmail.search("Date: ") + 6);
-	var emailDate = emailDateSub.substring(0, emailDateSub.indexOf('\\n'));
-	res.statusCode = 200;
-	res.end('email received\n');
-	if (emailSubject=="trampas"){
-		var mensaje1=String(emailBody);
-		var mensaje2=mensaje1.slice(1,-1);
-		var mensaje3= mensaje2.split("#");
-		Inbound.insert({
-			Body: emailBody,
-			Aldea: mensaje3[0],
-			Casa:mensaje3[1],
-			Sensor:mensaje3[2],
-			Date: emailDate,
-			Ack: false,
-		});
-	}
-}, {where: 'server'});
-
-T9n.setLanguage('es');
-T9n.map('es', {
-    "User not found": "Usuario no encontrado",
-    "Match failed": "Por favor introduzca su usuario y contraseña",
-    "Incorrect password": "Contraseña equivocada",
-    "Email already exists.": "Ya existe un usuario registrado con este correo",
-    "Need to set a username or email": "Por favor introduzca su direccion de correo",
-    "Password may not be empty": "Por favor introduzca su contraseña"
-});
+Markers = new Meteor.Collection("Markers");
 
 if (Meteor.isServer) {
-    if (Markers.find({}).count() === 0) {
-        Markers.insert({
-			lat: -25.305609,
-			lng: -57.560252,
-			map: "mapa1",
-
-        });
-        Markers.insert({
-            'translation': 'en_US',
-            'value1': 'translation1',
-            'value2': 'translation2'
-        });
-    }
 	Meteor.startup(function () {
 		Meteor.publish('Datos',function(){
 			//var currentUserId=this.userId;
@@ -149,7 +23,9 @@ if (Meteor.isServer) {
 			return Inbound.find()
 		});
 		Meteor.publish('Markers',function(){
-			return Markers.find()
+		//var currentUserId=this.userId;
+		//return Datos.find({createdBy: currentUserId})
+		return Markers.find()
 		});
 	});
 
@@ -175,86 +51,22 @@ if (Meteor.isServer) {
 		'ultimaDeteccion':function(){
 			return Inbound.findOne({}, {sort: {Date: -1, limit: 1}});
 		},
-		'insertarMarcador':function(uno){
-			Markers.insert(uno);
+		'insertarMarcador':function(marcadoor){
+			Markers.insert(marcadoor);
 		},
-		exportAllContacts: function() {		
-		var fields = [
-			"Direccion del Viento",
-			"Velocidad del Viente",
-			"Lluvia",
-			"Temperatura Exterior",
-			"Temperatura Interior",
-			"Humedad",
-			"Presion",
-			"GPS",
-			"Fuente",
-			"Estado",
-			"Fecha"			
-		];
- 
-		var data = [];		
- 
-		var logs = Datos.find().fetch();
-		_.each(logs, function(c) {
-			data.push([
-				c.dirviento,
-				c.velviento,
-				c.lluvia,				
-				c.temp_ext,
-				c.temp_int,
-				c.humedad,
-				c.presion,
-				c.gps,
-				c.fuente,
-				c.estado,
-				moment.utc(c.fecha).format("DD/MM/YYYY")
-			]);
-		});
- 
-		return {fields: fields, data: data};
+		'removeMarker': function(markerId){
+			Markers.remove(markerId);
 		},
-		exportAllContacts2: function() {		
-		var fields = [
-			"Aldea",
-			"Casa",
-			"Sensor",
-			"Fecha"		
-		];
- 
-		var data = [];		
- 
-		var logs = Inbound.find().fetch();
-		_.each(logs, function(c) {
-			data.push([
-				c.Aldea,
-				c.Casa,
-				c.Sensor,
-				c.Date
-			]);
-		});
- 
-		return {fields: fields, data: data};
-		},
-		'updateAck':function(id){
-			Inbound.update(id,{$set: {
-					Ack:true
-			}
+		'updateMarker':function(id,name,aldea,casa){
+			Markers.update(id,{$set:{
+					name:name,
+					aldea:aldea,
+					casa:casa
+				}
 			});
 		}
 	});
 
-	Inbound.find().observeChanges({
-	  added: function() {
-	    console.log("Se agrego algo a la BD!")
-	  },
-	  changed: function() {
-	    console.log("Se cambio algo de la BD!")
-	  },
-	  removed: function() {
-	    console.log("Se quito algo a la BD!")
-	  }
-	});
 }
 
 if (Meteor.isClient) {
@@ -287,17 +99,14 @@ if (Meteor.isClient) {
 		});
 		marker2.setIcon('http://maps.google.com/mapfiles/ms/icons/green-dot.png');
 		//Logs.findOne({}, {sort: {fecha: -1, limit: 1}}).fetch()
-		this.autorun(function() {
-				Meteor.call ("ultimaDeteccion", function(error,result){
-				if(error){
-						console.log(error);
-				} else{
-						if(result.Sensor=="01" && result.Ack==false){
-								Meteor.call("updateAck",result._id);
-								marker.setIcon('http://maps.google.com/mapfiles/ms/icons/red-dot.png');
-						}
+		Meteor.call ("ultimaDeteccion", function(error,result){
+			if(error){
+				console.log(error);
+			} else{
+				if(result.Sensor=="01"){
+					marker.setIcon('http://maps.google.com/mapfiles/ms/icons/red-dot.png');
 				}
-				});
+			}
 		});
 
 		marker.addListener('click', function() {
@@ -352,7 +161,6 @@ if (Meteor.isClient) {
 		}
 	});
 
-/*
 	Template.datos.onRendered(function(){
 		var temperatura=Router.current().params.query.temperatura;
 		var viento=Router.current().params.query.viento;
@@ -367,7 +175,6 @@ if (Meteor.isClient) {
 		};
 		Meteor.call("cargarDatosGet",ayuda);
 	});
-*/
 
 	Template.login.events({
 		'click #btn-signup': function(event){
@@ -465,103 +272,87 @@ if (Meteor.isClient) {
 	}
 	});
 
-	Template.exportarLogsEstacion.events({
- 		"click #export": function() {
- 			MyAppExporter.exportAllContacts();
- 		},
- 		"click #export2": function(){
- 			MyAppExporter.exportAllContacts2();
- 		}
- 	});
+	Template.marcadores.helpers({
+		'punto':function(){
+			return Markers.find();
+		}
+	});
 
- 	Template.agregarMarcador.events({
- 		"click #guardarNuevoMarcador": function(){
+	Template.marcadores.events({
+		'click #modifyMarker': function(){
+			$(".modal-body #modifyMarkerName").val(this.name);
+			$(".modal-body #modifyMarkerAldea").val(this.aldea);
+			$(".modal-body #modifyMarkerCasa").val(this.casa);
+			$(".modal-body #modifyMarkerId").val(this._id);
+		},
+		'click #removeMarker': function () {  
+			if (confirm("Estas seguro que desea borrar este marcador?") == true)
+				Meteor.call("removeMarker", this._id);
+		},
+		'keyup #filter': function (event) {
+			var rex = new RegExp($(event.target).val(), 'i');
+			$('.searchable tr').hide();
+			$('.searchable tr').filter(function () {
+				return rex.test($(this).text());
+			}) .show();
+		}
+	});
+	Template.marcadoresModal.events({
+		'click #saveClientModify': function(){
+			var name = $('#modifyMarkerName').val();
+			var aldea = $('#modifyMarkerAldea').val();
+			var casa = $('#modifyMarkerCasa').val();
+			var marcador = Markers.findOne($(".modal-body #modifyMarkerId").val());
+			var id = marcador._id;
+			if (name === "")
+				name = marcador.name;
+			if (aldea === "")
+				aldea = marcador.aldea;
+			if (casa === "")
+				casa = marcador.casa;
+			Meteor.call("updateMarker", id, name, aldea, casa);
+			$('#marcadoresModal').modal('hide'); 
+		},
+		'click #modifyClientLink': function() {
+			$('#clientModal').modal('hide');
+			$('body').removeClass('modal-open');
+			$('.modal-backdrop').remove();
+		}
+	});
+
+	Template.agregarMarcador.events({
+		'click #saveNewMarker':function(){
+			var markerName = $('.addMarker #newMarker').val();
+			var markerLat = parseFloat($('.addMarker #newMarkerLat').val());
+			var markerLong = parseFloat($('.addMarker #newMarkerLong').val());
+			var markerAldea = $('.addMarker #newMarkerAldea').val();
+			var markerCasa = $('.addMarker #newMarkerCasa').val();
+
 			var marcadorNuevo = {
- 				name: $('.nuevoMarcador #nuevoMarcadorNombre').val(),
- 				lat: parseFloat($('.nuevoMarcador #nuevoMarcadorLatitud').val()),
- 				long: parseFloat($('.nuevoMarcador #nuevoMarcadorLongitud').val()),
- 				aldea: $('.nuevoMarcador #nuevoMarcadorAldea').val(),
- 				casa: $('.nuevoMarcador #nuevoMarcadorCasa').val()
- 			};
+				name: markerName,
+				lat: markerLat,
+				long: markerLong,
+				aldea: markerAldea,
+				casa: markerCasa,
+			};
+			console.log(marcadorNuevo);
 
- 			console.log(marcadorNuevo);
- 			Meteor.call("insertarMarcador",marcadorNuevo);
- 			clearForm();
- 			$('#limpiarCampos').click();
- 		},
- 		"click #limpiarCampos":function(){
- 			clearForm();
- 		}
- 	});
+			Meteor.call("insertarMarcador",marcadorNuevo);
+			clearForm();
+			$('#clearFields').click();
+		},
+		'click #clearFields': function() {
+			clearForm(); 
+		}
+	});
+	
+	function clearForm() {
+    $('.addMarker #newMarker').val("");
+    $('.addMarker #newMarkerLat').val("");
+    $('.addMarker #newMarkerLong').val("");
+    $('.addMarker #newMarkerAldea').val("");
+    $('.addMarker #newMarkerCasa').val("");
+	}
 
- 	function clearForm() {
-	    $('.nuevoMarcador #nuevoMarcadorNombre').val("");
-	    $('.nuevoMarcador #nuevoMarcadorLatitud').val("");
-	    $('.nuevoMarcador #nuevoMarcadorLongitud').val("");
-	    $('.nuevoMarcador #nuevoMarcadorAldea').val("");
-	    $('.nuevoMarcador #nuevoMarcadorCasa').val("");
-	};
 
- 	MyAppExporter = {
- 		exportAllContacts: function() {
- 			var self = this;
- 			Meteor.call("exportAllContacts", function(error, data) {
-
- 				if ( error ) {
- 					alert(error); 
- 					return false;
- 				}
-
- 				var csv = Papa.unparse(data);
- 				self._downloadCSV(csv);
- 			});
- 		},
- 		exportAllContacts2: function() {
- 			var self = this;
- 			Meteor.call("exportAllContacts2", function(error, data) {
-
- 				if ( error ) {
- 					alert(error); 
- 					return false;
- 				}
-
- 				var csv = Papa.unparse(data);
- 				self._downloadCSV2(csv);
- 			});
- 		},
- 		exportContact: function(id) {
- 			var self = this;
- 			Meteor.call("exportContact", id, function(error, data) {
-
- 				if ( error ) {
- 					alert(error); 
- 					return false;
- 				}
-
- 				var csv = Papa.unparse(data);
- 				self._downloadCSV(csv);
- 			});
- 		},
-
- 		_downloadCSV: function(csv) {
- 			var blob = new Blob([csv]);
- 			var a = window.document.createElement("a");
- 			a.href = window.URL.createObjectURL(blob, {type: "text/plain"});
- 			fecha=moment.utc().format("DD/MM/YYYY");
- 			a.download = "Datos_Estacion_"+fecha+".csv";
- 			document.body.appendChild(a);
- 			a.click();
- 			document.body.removeChild(a);
- 		},
- 		_downloadCSV2: function(csv) {
- 			var blob = new Blob([csv]);
- 			var a = window.document.createElement("a");
- 			a.href = window.URL.createObjectURL(blob, {type: "text/plain"});
- 			fecha=moment.utc().format("DD/MM/YYYY");
- 			a.download = "Datos_Detecciones_" +fecha+".csv";
- 			document.body.appendChild(a);
- 			a.click();
- 			document.body.removeChild(a);
- 		}
- 	}
 }
