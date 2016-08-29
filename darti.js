1a1d46b638e6db19665e3055058be6dad806c523
Alexis Pojomovsky <apojomovsky@gmail.com>
2016-08-29 19:27:17 -0400

    Darti 3.0

--- darti.js
+++ darti.js
@@ -2,7 +2,10 @@
 Datos= new Meteor.Collection("Datos");
 Logs= new Meteor.Collection("Logs");
 Get= new Meteor.Collection("Get");
-
+var temperatura;
+var viento;
+var humedad;
+var presion;
 Router.configure({
 	layoutTemplate:'main'
 });
@@ -10,6 +13,18 @@ Router.configure({
 Router.route('/',{
 	name:'home',
 	template:'home'
+});
+
+Router.route('/datos',{
+	name:'datos',
+	template:'datos',
+	data: function () {
+      console.log(Router.current().params);
+      temperatura=Router.current().params.query.temperatura;
+      viento=Router.current().params.query.viento;
+      humedad=Router.current().params.query.humedad;
+      presion=Router.current().params.query.presion;
+    }
 });
 
 Router.route('/informes',{
@@ -70,30 +85,33 @@ if (Meteor.isServer) {
 				//console.log(message.snippet, message.to, message.from, message.subject);
 				console.log(message.snippet, message.from);
 				console.log(message.date);
-				console.log(typeof(message.date));
 				var mensaje=message.snippet;
 				var mensaje1=String(mensaje);
 				var mensaje2=mensaje1.slice(1,-1);
 				var mensaje3= mensaje2.split("#");
 				if (bandera===0){
 					Logs.insert({
+							ack:false,
 							aldea:mensaje3[0],
 							casa:mensaje3[1],
 							sensor:mensaje3[2],
 							mailId:message._id,
 							contenido:mensaje3,
-							remitente:message.from
+							remitente:message.from,
+							fecha:message.date
 						});
 					bandera=1;
 				}
 				else if (message._id!==Logs.findOne({mailId:message._id})){
 					Logs.insert({
+						ack:false,
 						aldea:mensaje3[0],
 						casa:mensaje3[1],
 						sensor:mensaje3[2],
 						mailId:message._id,
 						contenido:mensaje3,
-						remitente:message.from
+						remitente:message.from,
+						fecha:message.date
 					});
 				}
 			});
@@ -112,6 +130,14 @@ if (Meteor.isClient) {
 	Meteor.subscribe ('Logs');
 	Meteor.subscribe ('Get');
 
+	Datos.insert({
+		temp:temperatura,
+		vien:viento,
+		hume:humedad,
+		pres:presion,
+		fecha:new Date()
+	});
+
 	//Funcion para parsear los datos de los mails. Al final quedo en el server
 	/*function parseData(data){
     var newLog=data;
@@ -123,7 +149,7 @@ if (Meteor.isClient) {
   	}*/
 
 	Template.mapview1.onRendered(function () {
-		Logs.insert({
+		/*Logs.insert({
 			ack:false,
 			aldea:1,
 			casa:1,
@@ -134,7 +160,7 @@ if (Meteor.isClient) {
 			aldea:1,
 			casa:2,
 			sensor:1
-		});
+		});*/
 		var mapOptions = {
 			zoom: 16,
 			center: new google.maps.LatLng(-25.304251, -57.560504)
@@ -157,19 +183,38 @@ if (Meteor.isClient) {
 		});
 		marker2.setIcon('http://maps.google.com/mapfiles/ms/icons/green-dot.png');
 		//Rutina de evento "click" en el mapa
-		/*if (Logs.ack===true)
+		var ultimaDeteccion= Logs.findOne({}, {sort: {fecha: -1, limit: 1}});
+		console.log();
+		if(Logs.find().count()!=0){
+			if(ultimaDeteccion.aldea===1 && ultimaDeteccion.casa===1){
+			marker.setIcon('http://maps.google.com/mapfiles/ms/icons/red-dot.png');
+			}
+			if(ultimaDeteccion.aldea===1 && ultimaDeteccion.casa===2){
+			marker2.setIcon('http://maps.google.com/mapfiles/ms/icons/red-dot.png');
+			}
+		}
 		marker.addListener('click', function() {
-    	marker.setIcon('http://maps.google.com/mapfiles/ms/icons/green-dot.png');
+			Logs.update(ultimaDeteccion._id,{
+				$set:{ack:true},
+			});
+    		marker.setIcon('http://maps.google.com/mapfiles/ms/icons/green-dot.png');
+  		});
+  		marker2.addListener('click', function() {
+	    	marker2.setIcon('http://maps.google.com/mapfiles/ms/icons/green-dot.png');
+  		});
+	}); 
 
-  		});*/
-	});  
+	Template.mapview1.helpers({
+
+	});
+
 	Template.mapview2.onRendered(function () {
-		Logs.insert({
+		/*Logs.insert({
 			ack:false,
 			aldea:2,
 			casa:1,
 			sensor:1
-		});
+		});*/
 		var mapOptions = {
 			zoom: 16,
 			center: new google.maps.LatLng(-25.342175, -57.625492)
@@ -187,12 +232,12 @@ if (Meteor.isClient) {
 		marker.setIcon('http://maps.google.com/mapfiles/ms/icons/green-dot.png');
 	});  
 	Template.mapview3.onRendered(function () {
-		Logs.insert({
+		/*Logs.insert({
 			ack:false,
 			aldea:3,
 			casa:1,
 			sensor:1
-		});
+		});*/
 		var mapOptions = {
 			zoom: 16,
 			center: new google.maps.LatLng(-25.093469, -57.521271)
@@ -214,27 +259,8 @@ if (Meteor.isClient) {
 		'log':function(){
 			return Logs.find({}, {sort: {createdAt: -1} });
 		},
-		'getInfo':function(){
-			/*HTTP.call( 'GET', 'http://jsonplaceholder.typicode.com/posts', {}, function( error, response ) {
-			//HTTP.call( 'GET', 'file:///home/carlos/Meteor/Darti/getInfo.json', {}, function( error, response ) {
-				if ( error ) {
-					console.log( error );
-				} else {
-					console.log( response );
-	 			}
-			});*/
-			HTTP.call( 'GET', 'http://jsonplaceholder.typicode.com/posts', {
-				params: {
-				"id": 5
-				}
-			}, function( error, response ) {
-				if ( error ) {
-					console.log( error );
-				} else {
-					console.log( response );
-					return response;
- 				}
-			});
+		'temperatura':function(){
+			return temperatura;
 		}
 	});
 }
